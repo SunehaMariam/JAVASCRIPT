52e5748b4ecbcf0a8b480cabe03f6e21f8516faf
SunehaMariam <sunehamariam475@gmail.com>
2024-10-26 12:08:35 -0700

    Update chapter-3.js

--- chapter-3.js
+++ chapter-3.js
@@ -1,12 +1,12 @@
-// var age = 19;
-// alert(age);
-// var visitCount =+prompt()
-// var visit = ("you have visited this site for" +" "+ visitCount+" "+   "times")
-// alert(visit)
+var age = 19;
+alert(age);
+var visitCount =+prompt()
+var visit = ("you have visited this site for" +" "+ visitCount+" "+   "times")
+alert(visit)
 
-// var birthYear = 2005;
-// document.write("My birth year is"+" "+birthYear +"<br>");
+var birthYear = 2005;
+document.write("My birth year is"+" "+birthYear +"<br>");
 
-//  var VisitorsName = "Suneha"
-//  var quantity = "5 T-shirt(s)";
-// document.write(VisitorsName +" "+ "ordered" +" "+ quantity+" "+ "on XYZ clothing store");
+ var VisitorsName = "Suneha"
+ var quantity = "5 T-shirt(s)";
+document.write(VisitorsName +" "+ "ordered" +" "+ quantity+" "+ "on XYZ clothing store");
