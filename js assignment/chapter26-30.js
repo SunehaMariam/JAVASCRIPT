ec910fadfdd30ae2c565b21ee4eaaab5bf9d9bed
SunehaMariam <sunehamariam475@gmail.com>
2025-01-22 14:19:15 +0500

    Create chapter26-30.js

---
+++ js assignment/chapter26-30.js
@@ -0,0 +1,49 @@
+// let number =3.45214
+// console.log(Math.round(number));
+// console.log(Math.floor(number));
+// console.log(Math.ceil(number));
+// QUESTION:02
+// let number = -2.673
+// console.log(Math.round(number));
+// console.log(Math.floor(number));
+// console.log(Math.ceil(number));
+
+// Question:03
+
+// Prompt the user to input a number
+// let number = prompt("Enter a number:");
+// number = parseFloat(number)
+// let absoluteValue = Math.abs(number);
+// document.write("The absolute value of " + number + " is " + absoluteValue);
+// Question:04
+// var diceRoll = Math.round(Math.random()*5)+1;
+// document.write("Diceroll: ",diceRoll)
+
+// QUESTION:05
+// let tossCoin = Math.random() < 0.5 ? 'Head' : 'Tails';
+// let tossCoinNum = Math.random();
+// if (tossCoinNum < 0.5) {
+//     document.write("Head");
+// } else {
+//     document.write("Tails");
+// }
+// QUESTION:06
+// var number = Math.round(Math.random()*100);
+// document.write("Random number between 1 and 100: ",number)
+// QUESTION:07
+// let weight = prompt("Enter your weight (e.g., 50, 50kgs, 50.2kgs, 50.2kilograms):");
+// let weightInKgs = parseFloat(weight);
+// if (!isNaN(weightInKgs)) {
+//   console.log(`Your weight is`,weightInKgs, `kgs.`);
+// } else {
+//   console.log("Invalid input. Please enter a valid weight.");
+// }
+// QUESTION:08
+// let secretNumber = Math.floor(Math.random() * 10) + 1;
+// let userGuess = prompt("Guess a number between 1 and 10:");
+// userGuess = parseInt(userGuess);
+// if (userGuess === secretNumber) {
+//     document.write("Congratulations! You guessed the secret number: " + secretNumber);
+// } else {
+//     document.write("Sorry! The secret number was " + secretNumber + ". Better luck next time!");
+// }
